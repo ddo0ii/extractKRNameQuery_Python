a68cab07f13bc6703d7e08cc76e5728be7e547ab
soyhandong <21500035@handong.edu>
2022-05-23 14:08:05 +0900

    Nature_추출링_Python

---
+++ Nature/Nature_Health_removing.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+import pandas as pd 
+import numpy as np
+import os
+os.system("python Nature_Health.py")
+
+# making data frame from csv file 
+data = pd.read_csv("Nature_Health.csv", encoding="utf-8-sig", engine='python') 
+  
+# dropping ALL duplicte values 
+data.drop_duplicates(subset ="Headline",keep = "last", inplace = True) 
+  
+# displaying data 
+print(data)
+
+data.to_csv("Nature_Health_result.csv")
